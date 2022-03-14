23a602a92087d2d2d3fb6fba2881ed405b4599cb
devivoc <55997843+devivoc@users.noreply.github.com>
2022-03-14 17:18:31 -0400

    created project and Advertiser class

---
+++ Multipeer Familiarization/Multipeer Familiarization/Multipeer_FamiliarizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multipeer_FamiliarizationApp.swift
+//  Multipeer Familiarization
+//
+//  Created by Christian DeVivo on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Multipeer_FamiliarizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
